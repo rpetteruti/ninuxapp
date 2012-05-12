c781a2c1715deb33c3082cd94abf652e5c691577
Raffaele Petteruti <info@neomaru.it>
2012-05-12 15:34:20 +0200

    first commit

---
+++ ninuxapp/ninuxapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ninuxapp
+//
+//  Created by Neo on 12/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ ninuxapp/ninuxapp/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ninuxapp
+//
+//  Created by Neo on 12/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
