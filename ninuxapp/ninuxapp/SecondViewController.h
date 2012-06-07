4954ba88dfebc98409740d7f0bb3f12e59a5bcc2
Raffaele Petteruti <eidos85@gmail.com>
2012-06-07 19:50:46 +0200

    Added database and now the app load the nodes from the sqlite database

--- ninuxapp/ninuxapp/SecondViewController.h
+++ ninuxapp/ninuxapp/SecondViewController.h
@@ -5,15 +5,21 @@
 //  Created by Neo on 12/05/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
+#define pathDB @"nodes.sqlite"
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 #import "SBJson.h"
+#import <sqlite3.h>
+
+static sqlite3 *database = nil;
 
 @interface SecondViewController : UIViewController{
     
-
-    
+    IBOutlet MKMapView *map;
+    NSString *writableDBPath;
     
 }
+-(void)populateMap;
 
 @end
