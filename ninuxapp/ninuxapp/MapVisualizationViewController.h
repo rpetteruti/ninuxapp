a1f2e92b9465752d9171bfdbebfae2d2097c5a2e
Raffaele Petteruti <info@neomaru.it>
2012-06-12 22:53:34 +0200

    added new version of MapVisualizationVC

---
+++ ninuxapp/ninuxapp/MapVisualizationViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapVisualizationViewController.h
+//  ninuxapp
+//
+//  Created by Mara Sorella on 12/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapVisualizationViewController : UIViewController
+
+@end
