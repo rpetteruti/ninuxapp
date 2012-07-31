e0d26fa4c70d9c01857227b3eec63362cb88e290
Raffaele Petteruti <eidos85@gmail.com>
2012-07-31 11:25:53 +0200

    Added push notification code to remote register the device

--- ninuxapp/ninuxapp/AppDelegate.m
+++ ninuxapp/ninuxapp/AppDelegate.m
@@ -67,11 +67,109 @@
     NSLog(@"Device Token pretty: %@",[[[devToken description] 
                                        stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@""]] 
                                       stringByReplacingOccurrencesOfString:@" " withString:@""]);
-    //NSLog(@"Device Token : %@", self.currentDeviceToken);
-    NSLog(@"Remote type : %d", [[UIApplication sharedApplication] enabledRemoteNotificationTypes]);
     
     
-    NSLog(@"Notification token taken");
+#if !TARGET_IPHONE_SIMULATOR
+    
+	NSLog(@"Did register for remote notifications: %@", devToken);
+    
+    
+	// Get Bundle Info for Remote Registration (handy if you have more than one app)
+	NSString *appName = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"];
+	NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+    
+	// Check what Notifications the user has turned on.  We registered for all three, but they may have manually disabled some or all of them.
+	NSUInteger rntypes = [[UIApplication sharedApplication] enabledRemoteNotificationTypes];
+    
+	// Set the defaults to disabled unless we find otherwise...
+	NSString *pushBadge = @"disabled";
+	NSString *pushAlert = @"disabled";
+	NSString *pushSound = @"disabled";
+    
+	// Check what Registered Types are turned on. This is a bit tricky since if two are enabled, and one is off, it will return a number 2... not telling you which
+	// one is actually disabled. So we are literally checking to see if rnTypes matches what is turned on, instead of by number. The "tricky" part is that the
+	// single notification types will only match if they are the ONLY one enabled.  Likewise, when we are checking for a pair of notifications, it will only be
+	// true if those two notifications are on.  This is why the code is written this way
+	if(rntypes == UIRemoteNotificationTypeBadge){
+		pushBadge = @"enabled";
+	}
+	else if(rntypes == UIRemoteNotificationTypeAlert){
+		pushAlert = @"enabled";
+	}
+	else if(rntypes == UIRemoteNotificationTypeSound){
+		pushSound = @"enabled";
+	}
+	else if(rntypes == ( UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert)){
+		pushBadge = @"enabled";
+		pushAlert = @"enabled";
+	}
+	else if(rntypes == ( UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)){
+		pushBadge = @"enabled";
+		pushSound = @"enabled";
+	}
+	else if(rntypes == ( UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound)){
+		pushAlert = @"enabled";
+		pushSound = @"enabled";
+	}
+	else if(rntypes == ( UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound)){
+		pushBadge = @"enabled";
+		pushAlert = @"enabled";
+		pushSound = @"enabled";
+	}
+    
+	// Get the users Device Model, Display Name, Unique ID, Token & Version Number
+	UIDevice *dev = [UIDevice currentDevice];
+	NSString *deviceUuid = dev.uniqueIdentifier;
+    NSString *deviceName = dev.name;
+	NSString *deviceModel = dev.model;
+	NSString *deviceSystemVersion = dev.systemVersion;
+    
+	// Prepare the Device Token for Registration (remove spaces and < >)
+	NSString *deviceToken = [[[[devToken description]
+                               stringByReplacingOccurrencesOfString:@"<"withString:@""]
+                              stringByReplacingOccurrencesOfString:@">" withString:@""]
+                             stringByReplacingOccurrencesOfString: @" " withString: @""];
+    
+	// Build URL String for Registration
+	// !!! CHANGE "www.mywebsite.com" TO YOUR WEBSITE. Leave out the http://
+	// !!! SAMPLE: "secure.awesomeapp.com"
+	NSString *host = @"push.formiacloud.com";
+    
+	// !!! CHANGE "/apns.php?" TO THE PATH TO WHERE apns.php IS INSTALLED
+	// !!! ( MUST START WITH / AND END WITH ? ).
+	// !!! SAMPLE: "/path/to/apns.php?"
+	NSString *urlString = [@"/apns.php?"stringByAppendingString:@"task=register"];
+    
+	urlString = [urlString stringByAppendingString:@"&appname="];
+	urlString = [urlString stringByAppendingString:appName];
+	urlString = [urlString stringByAppendingString:@"&appversion="];
+	urlString = [urlString stringByAppendingString:appVersion];
+	urlString = [urlString stringByAppendingString:@"&deviceuid="];
+	urlString = [urlString stringByAppendingString:deviceUuid];
+	urlString = [urlString stringByAppendingString:@"&devicetoken="];
+	urlString = [urlString stringByAppendingString:deviceToken];
+	urlString = [urlString stringByAppendingString:@"&devicename="];
+	urlString = [urlString stringByAppendingString:deviceName];
+	urlString = [urlString stringByAppendingString:@"&devicemodel="];
+	urlString = [urlString stringByAppendingString:deviceModel];
+	urlString = [urlString stringByAppendingString:@"&deviceversion="];
+	urlString = [urlString stringByAppendingString:deviceSystemVersion];
+	urlString = [urlString stringByAppendingString:@"&pushbadge="];
+	urlString = [urlString stringByAppendingString:pushBadge];
+	urlString = [urlString stringByAppendingString:@"&pushalert="];
+	urlString = [urlString stringByAppendingString:pushAlert];
+	urlString = [urlString stringByAppendingString:@"&pushsound="];
+	urlString = [urlString stringByAppendingString:pushSound];
+    
+	// Register the Device Data
+	// !!! CHANGE "http" TO "https" IF YOU ARE USING HTTPS PROTOCOL
+	NSURL *url = [[NSURL alloc] initWithScheme:@"http" host:host path:urlString];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+	NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+	NSLog(@"Register URL: %@", url);
+	NSLog(@"Return Data: %@", returnData);
+    
+#endif
     
 }
 
@@ -95,5 +193,53 @@
 {
 }
 */
+
+/**
+ * Remote Notification Received while application was open.
+ */
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    
+#if !TARGET_IPHONE_SIMULATOR
+    
+    application.applicationIconBadgeNumber = 0;
+    //self.textView.text = [userInfo description];
+    
+    // We can determine whether an application is launched as a result of the user tapping the action
+    // button or whether the notification was delivered to the already-running application by examining
+    // the application state.
+    
+    if (application.applicationState == UIApplicationStateActive) {
+        // Nothing to do if applicationState is Inactive, the iOS already displayed an alert view.
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Did receive a Remote Notification"
+                                                            message:[NSString stringWithFormat:@"The application received this remote notification while it was running:\n%@",
+                                                                     [[userInfo objectForKey:@"aps"] objectForKey:@"alert"]]
+                                                           delegate:self
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+        [alertView show];
+        
+    }
+    
+	NSLog(@"remote notification: %@",[userInfo description]);
+	NSDictionary *apsInfo = [userInfo objectForKey:@"aps"];
+    
+	NSString *alert = [apsInfo objectForKey:@"alert"];
+	NSLog(@"Received Push Alert: %@", alert);
+    
+	NSString *sound = [apsInfo objectForKey:@"sound"];
+	NSLog(@"Received Push Sound: %@", sound);
+    
+	NSString *badge = [apsInfo objectForKey:@"badge"];
+	NSLog(@"Received Push Badge: %@", badge);
+    
+#endif
+    
+}
+
+/*
+ * ---------------------------------------------------------------------
+ *  END APNS CODE
+ * ---------------------------------------------------------------------
+ */
 
 @end
